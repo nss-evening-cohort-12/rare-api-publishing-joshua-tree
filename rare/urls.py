ea792d3590951c5cd43f9fca54b8bd6481d29dbb
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-12-08 19:11:55 -0600

    wip

--- rare/urls.py
+++ rare/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include
 from django.urls import path
-from rareapi.views import login_user
+from rareapi.views import login_user, register_user
 
 urlpatterns = [
-    # path('register', register_user),
+    path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- rareapi/views/auth.py
+++ rareapi/views/auth.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
-# from rareapi.models import Gamer
+from rareapi.models import RareUser
 
 
 @csrf_exempt
@@ -35,3 +35,35 @@ def login_user(request):
             # Bad login details were provided. So we can't log the user in.
             data = json.dumps({"valid": False})
             return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def register_user(request):
+    '''Handles the creation of a new gamer for authentication
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+
+    req_body = json.loads(request.body.decode())
+
+    new_user = User.objects.create_user(
+        username=req_body['username'],
+        email=req_body['email'],
+        password=req_body['password'],
+        first_name=req_body['first_name'],
+        last_name=req_body['last_name']
+    )
+
+    rare_user = RareUser.objects.create(
+        bio=req_body['bio'],
+        user=new_user
+    )
+
+    rare_user.save()
+
+
+    token = Token.objects.create(user=new_user)
+
+
+    data = json.dumps({"token": token.key})
+    return HttpResponse(data, content_type='application/json')
