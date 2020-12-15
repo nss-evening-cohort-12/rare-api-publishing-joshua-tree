2322ededb4b6df10fcfc7f19d2b8f887ac059b7f
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-12-14 18:20:30 -0600

    wip

--- rare/urls.py
+++ rare/urls.py
@@ -4,23 +4,15 @@ from django.contrib import admin
 from rareapi.views import login_user, register_user
 from django.conf.urls import include
 from rest_framework import routers
-<<<<<<< HEAD
-from rareapi.views import CategoriesViewSet, Tags, Comments
-
-=======
-from rareapi.views import CategoriesViewSet, Tags, PostsViewSet, UsersViewSet
->>>>>>> main
+from rareapi.views import CategoriesViewSet, Tags, Comments, PostsViewSet, UsersViewSet
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoriesViewSet, 'category')
 router.register(r'tags', Tags, 'tag')
-<<<<<<< HEAD
 router.register(r'comments', Comments, 'comment')
-=======
 router.register(r'posts', PostsViewSet, 'post')
 router.register(r'users', UsersViewSet, 'user')
->>>>>>> main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
