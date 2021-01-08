530c69c9359b009fa31158a7e0ef4bd6fb1c2e38
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-01-07 19:03:42 -0600

    viewset and serializer for auth users

---
+++ rareapi/views/authuser.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import status, viewsets, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rareapi.models import RareUser
+
+class UserSerializer(serializers.ModelSerializer):
+    # JSON Serializer for User
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
+
+class AuthUserViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
--- rareapi/views/user.py
+++ rareapi/views/user.py
@@ -1,9 +1,10 @@
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
-from rest_framework import serializers, status
+from rest_framework import serializers, status, viewsets
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rareapi.models import RareUser
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     # JSON Serializer for User
@@ -33,3 +34,10 @@ class UsersViewSet(ViewSet):
         serializer = UserSerializer(
             users, many=True, context={'request': request})
         return Response(serializer.data)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
